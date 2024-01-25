2f57a6cbfae0213c5fe8fbcf7752c21540353051
yudinaan <yudinaanastasiya01@mail.ru>
2024-01-26 00:57:56 +0500

    Initial commit

---
+++ app/src/main/java/com/example/recipes/Item.kt
@@ -0,0 +1,5 @@
+package com.example.recipes
+
+class Item(val id: Int, val image: String,val  title: String,val  desc: String,val text: String) {
+
+}
---
+++ app/src/main/java/com/example/recipes/ItemActivity.kt
@@ -0,0 +1,18 @@
+package com.example.recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+
+        val title: TextView = findViewById((R.id.item_list_title_one))
+        val text: TextView = findViewById(R.id.item_list_text)
+
+        title.text = intent.getStringExtra("itemTitle")
+        text.text = intent.getStringExtra("itemText")
+    }
+}
